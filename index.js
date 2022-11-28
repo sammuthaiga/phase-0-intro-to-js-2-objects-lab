530b168abae2af21405021df550409a33c08d27f
Samuel <samuel.muthaiga@student.moringaschool.com>
2022-11-28 20:04:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+const employee = {};
+employee.name = 'Samido';
+employee.streetAddress = 'Embakasi'
+
+employee;
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value
+    return newEmployee
+}
+const nOnDestructiveUpdatedEmployee = updateEmployeeWithKeyAndValue(employee, "name", "sam")
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+const destructiveEmployeeUpdates = destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "sam")
+console.log(destructiveEmployeeUpdates);
+
+function deleteFromEmployeeByKey(employee, key){
+    const employeeCopy = {...employee}
+    delete employeeCopy["name"]
+
+    return employeeCopy;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    const newEmployee = delete employee["name"]
+    return newEmployee;
+}
